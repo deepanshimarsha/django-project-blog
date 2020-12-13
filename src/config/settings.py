ef60dfdd3eead280981eba7e0ceabd6b5288c791
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2020-12-13 19:41:28 +0530

    install django rest framework and include it in config settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party
+    'rest_framework'
     #first party
     'blog.apps.BlogConfig',
     'organizer.apps.OrganizerConfig',
+
 ]
 
 MIDDLEWARE = [
