f785e2a5945fffcfb5454d8f35e24c1a1c973cc5
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2021-01-18 14:28:44 +0530

    configure URL paths
    To differentiate between Api URI's and traditional URL's

--- src/config/urls.py
+++ src/config/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.urls import include, path
-from organizer import urls as organizer_urls
+from organizer.routers import urlpatterns as organizer_api_urls
+from blog.routers import urlpatterns as blog_api_urls
+
+api_urls = organizer_api_urls + blog_api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include(organizer_urls)),
+    path("api/v1/", include(api_urls)),
+
 ]
