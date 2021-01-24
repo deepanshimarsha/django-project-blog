2c0d5680bac136ed9481d08acca2e0f3a36be494
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2021-01-24 20:27:15 +0530

    simplify the API code with viewsets and routers

--- src/organizer/routers.py
+++ src/organizer/routers.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 from organizer import views
+from .viewsets import TagViewSet
 
-urlpatterns = [
-    path("tag/", views.TagApiList.as_view(), name="api-tag-list"),
-    path("tag/<str:slug>/", views.TagApiDetail.as_view(), name="api-tag-detail"),
+api_router = SimpleRouter()
+api_router.register("tag", TagViewSet, basename="api-tag")
+api_routes = api_router.urls
+
+urlpatterns = api_routes + [
+
     path("startup/", views.StartupApiList.as_view(), name="api-startup-list"),
     path("startup/<str:slug>/", views.StartupApiDetail.as_view(), name="api-startup-detail"),
     path("newslink/", views.NewsLinkApiList.as_view(), name="api-newslink-list"),
