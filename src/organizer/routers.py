0ff28f39d78290f06077b13999494bf67f5fa5de
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2021-02-01 23:33:27 +0530

    create router for newslink
    override the SimpleRouter function because a newslink is identified by two fields

--- src/organizer/routers.py
+++ src/organizer/routers.py
@@ -1,17 +1,43 @@
-from django.urls import path
+"""URL Paths and Routers for Organizer App"""
 from rest_framework.routers import SimpleRouter
-from organizer import views
-from .viewsets import TagViewSet, StartupViewSet
 
+from .viewsets import (
+    NewsLinkViewSet,
+    StartupViewSet,
+    TagViewSet,
+)
+
+
+class NewsLinkRouter(SimpleRouter):
+    """Override the SimpleRouter for articles
+    DRF's routers expect there to only be a single variable
+    for finding objects. However, our NewsLinks needs
+    two! We therefore override the Router's behavior to
+    make it do what we want.
+    The big question: was it worth switching to a ViewSet
+    and Router over our previous config for this?
+    """
+
+    def get_lookup_regex(self, *args, **kwargs):
+        """Return regular expression pattern for URL path
+        This is the (rough) equivalent of the simple path:
+            <str:startup_slug>/<str:newslink_slug>
+        """
+        return (
+            r"(?P<startup_slug>[^/.]+)/"
+            r"(?P<newslink_slug>[^/.]+)"
+        )
 
 
 api_router = SimpleRouter()
 api_router.register("tag", TagViewSet, base_name="api-tag")
-api_router.register("startup", StartupViewSet, base_name="api-startup")
-api_routes = api_router.urls
+api_router.register(
+    "startup", StartupViewSet, base_name="api-startup"
+)
 
-urlpatterns = api_routes + [
+nl_router = NewsLinkRouter()
+nl_router.register(
+    "newslink", NewsLinkViewSet, base_name="api-newslink"
+)
 
-    path("newslink/", views.NewsLinkApiList.as_view(), name="api-newslink-list"),
-    path("newslink/<str:startup_slug>/<str:newslink_slug>/", views.NewsLinkApiDetail.as_view(), name="api-newslink-detail"),
-]
+urlpatterns = api_router.urls + nl_router.urls
